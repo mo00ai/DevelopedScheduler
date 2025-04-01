f3bd50974182a1d2b9ba65eb6930b08d7563e834
AhyunCho <jah112482@gmail.com>
2025-04-01 12:58:26 +0900

    init

---
+++ src/main/java/com/example/developedscheduler/DevelopedSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.example.developedscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelopedSchedulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevelopedSchedulerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/developedscheduler/DevelopedSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.developedscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevelopedSchedulerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
